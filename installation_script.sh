353a5001eb55e50ddb63ba6a67e2b7585f29e24f
SafeSquid-SWG <100831153+SafeSquid-SWG@users.noreply.github.com>
2022-03-16 15:26:36 +0530

    Update installation_script.sh

--- installation_script.sh
+++ installation_script.sh
@@ -4,7 +4,7 @@ apt update && apt upgrade -y
 apt install bind9 monit -y
 /etc/init.d/monit stop
 /etc/init.d/safesquid stop
-# sudo curl -L http://downloads.safesquid.net/appliance/binary/safesquid_latest.tar.gz -o /tmp/safesquid_latest.tar.gz
+sudo curl -L http://downloads.safesquid.net/appliance/binary/safesquid_latest.tar.gz -o /tmp/safesquid_latest.tar.gz
 sudo tar -xvzf /tmp/safesquid_latest.tar.gz
 /usr/local/src/_mkappliance/installation/setup.sh
 ldd /opt/safesquid/bin/safesquid
