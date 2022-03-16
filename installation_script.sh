13766e250f22418d6b65b3c451bdb054a42c74b9
SafeSquid-SWG <100831153+SafeSquid-SWG@users.noreply.github.com>
2022-03-16 15:35:28 +0530

    Add files via upload
    Installing SafeSquid via tarball

---
+++ installation_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install bind9 monit -y
+/etc/init.d/monit stop
+/etc/init.d/safesquid stop
+curl -L http://downloads.safesquid.net/appliance/binary/safesquid_latest.tar.gz -o /tmp/safesquid_latest.tar.gz
+tar -xvzf /tmp/safesquid_latest.tar.gz
+source /tmp/_mkappliance/installation/setup.sh
+ldd /opt/safesquid/bin/safesquid
+/etc/init.d/safesquid start
+netstat -tulnp | grep "safesquid"
+echo
+echo 
+echo " safe squid has installed!! "
